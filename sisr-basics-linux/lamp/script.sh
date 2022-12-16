5a4be2eb2a8803da5cab049535ff0926d18e3ed5
ellgt <120686750+ellgt@users.noreply.github.com>
2022-12-16 20:19:36 +0100

    Update script.sh

--- sisr-basics-linux/lamp/script.sh
+++ sisr-basics-linux/lamp/script.sh
@@ -2,6 +2,21 @@
 sudo apt update
 sudo apt-get -y install mysql-server mysql-client
 
-sudo apt-get -y install bind9 apache2 php libapache2-mod-php openssl php-curl php-gd php-php-gettext php-ldap php-intl php-json php-mysql php-mbstring php-xml php-zip php-imagick php-imap php-apcu php-cli php-fpm php-cgi
+sudo apt-get -y install curl bind9 ghostscript apache2 php libapache2-mod-php openssl php-curl php-gd php-php-gettext php-ldap php-intl php-json php-mysql php-mbstring php-xml php-zip php-imagick php-imap php-apcu php-cli php-bcmath php-fpm php-cgi open-vm-tools open-vm-tools-desktop
 clear
 
+echo ""
+echo "           \\\||||||////"
+echo "            \\  ~ ~  //"
+echo "             (  @ @  )"
+echo "    ______ oOOo-(_)-oOOo___________"
+echo "    Install finish"
+echo "    *******************************"
+echo "    Thank's for have use sisr-basics"
+echo "    _____________Oooo._____________"
+echo "       .oooO     (   )"
+echo "        (   )     ) /"
+echo "         \ (     (_/"
+echo "          \_)"
+echo ""
+echo ""
