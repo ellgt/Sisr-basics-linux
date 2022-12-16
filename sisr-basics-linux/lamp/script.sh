523d4997688e80c2ca6ba1c1f572f48d0e494946
ellgt <120686750+ellgt@users.noreply.github.com>
2022-12-16 20:16:39 +0100

    Update script.sh

--- sisr-basics-linux/lamp/script.sh
+++ sisr-basics-linux/lamp/script.sh
@@ -5,34 +5,3 @@ sudo apt-get -y install mysql-server mysql-client
 sudo apt-get -y install bind9 apache2 php libapache2-mod-php openssl php-curl php-gd php-php-gettext php-ldap php-intl php-json php-mysql php-mbstring php-xml php-zip php-imagick php-imap php-apcu php-cli php-fpm php-cgi
 clear
 
-menu()
-{
-echo "*************************************************************"
-echo "* the install is complete !                                 *"
-echo "*                                                           *"
-echo "* Do you want to quit or get back to the menu ?             *"
-echo "*                                                           *"
-echo "*          1)Quit                           2)Menu          *"
-echo "*************************************************************"
-}
-echo ""
-menu
-echo " Enter your choice :"
-echo ""
-echo ""
-read choice
-
-	case $choice in
-		1)
-			cd ..
-			sudo chmod +x quit.sh
-			sudo ./quit.sh
-			;;
-		2)
-			cd ..
-			sudo ./install.sh
-			;;
-	esac
-exit
-
-
