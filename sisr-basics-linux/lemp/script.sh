7215ff237870309b97bbdbeefb0a3773b44219fd
ellgt <120686750+ellgt@users.noreply.github.com>
2022-12-16 20:27:49 +0100

    Update script.sh

--- sisr-basics-linux/lemp/script.sh
+++ sisr-basics-linux/lemp/script.sh
@@ -2,37 +2,21 @@
 sudo apt update
 sudo apt-get -y install mysql-server mysql-client
 
-sudo apt-get -y install bind9 nginx php openssl php-curl php-gd php-php-gettext php-ldap php-intl php-json php-mysql php-mbstring php-xml php-zip php-imagick php-imap php-apcu php-cli php-fpm php-cgi
+sudo apt-get -y install bind9 nginx php sudo apt-get -y install curl bind9 ghostscript apache2 php libapache2-mod-php openssl php-curl php-gd php-php-gettext php-ldap php-intl php-json php-mysql php-mbstring php-xml php-zip php-imagick php-imap php-apcu php-cli php-bcmath php-fpm php-cgi open-vm-tools open-vm-tools-desktop
 sudo ufw allow 'Nginx HTTP'
 clear
-menu()
-{
-echo "*************************************************************"
-echo "* the install is complete !                                 *"
-echo "*                                                           *"
-echo "* Do you want to quit or get back to the menu ?             *"
-echo "*                                                           *"
-echo "*          1)Quit                           2)Menu          *"
-echo "*************************************************************"
-}
 echo ""
-menu
-echo " Enter your choice :"
+echo "           \\\||||||////"
+echo "            \\  ~ ~  //"
+echo "             (  @ @  )"
+echo "    ______ oOOo-(_)-oOOo___________"
+echo "    Install finish"
+echo "    *******************************"
+echo "    Thank's for have use sisr-basics"
+echo "    _____________Oooo._____________"
+echo "       .oooO     (   )"
+echo "        (   )     ) /"
+echo "         \ (     (_/"
+echo "          \_)"
 echo ""
 echo ""
-read choice
-
-	case $choice in
-		1)
-			cd ..
-			sudo chmod +x quit.sh
-			sudo ./quit.sh
-			;;
-		2)
-			cd ..
-			sudo ./install.sh
-			;;
-	esac
-exit
-
-
